9e4b33d0851f8cca9f0d1a8955a8e63d2a7f9c9f
washerzdotcom <143495195+washerzdotcom@users.noreply.github.com>
2023-10-05 13:33:35 +0530

    respo in billcustomer section

--- src/Components/BillCustomerInfo.jsx
+++ src/Components/BillCustomerInfo.jsx
@@ -1,6 +1,7 @@
 import { Center } from '@chakra-ui/react'
 import React, { useContext } from 'react'
 import { AppContext } from '../utills/context'
+import "../style/responsive.css"
 
 const BillCustomerInfo = (currObj) => {
   const {currObj: {customerName, contactNo, address} } = useContext(AppContext);
@@ -10,9 +11,9 @@ const BillCustomerInfo = (currObj) => {
  {/* <h5 style={{paddingLeft: '20px', paddingTop:'10px', paddingBottom:"10px"}}>Customer Info:</h5> */}
         <div
           style={{
-            display: "flex",
-            justifyContent: "space-around",
-            alignItems:"center",
+            // display: "flex",
+            // justifyContent: "space-around",
+            // alignItems:"center",
             border: "2px solid black",
             borderRadius: "5px",
             padding: "20px",
@@ -20,14 +21,19 @@ const BillCustomerInfo = (currObj) => {
             margin:"10px",
           }}
         >
-          <div>
-            <p>Name:</p>
-            <p>Contact:</p>
-            <p>Address:</p>
-          </div>
-          <div>
+          <div className='name' style={{display:"inline-block"}}>
+            <h6>Name:</h6>
             <p>{customerName}</p>
-            <p>{contactNo}</p>
+          </div>
+
+          <div className='name'>
+            <h6>Contact:</h6>
+            <p >{contactNo}</p>
+          </div>
+
+
+          <div className='name'>
+            <h6>Address:</h6>
             <p>{address}</p>
           </div>
         </div>
